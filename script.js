31b001179de52d3f79b154d78fd6fe43cd5d4959
insertSoul <j.clarke213@gmail.com>
2023-02-06 12:51:15 +0000

    Change the graphic to a square, add colour changes

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.onload = function() {
     
         src.connect(analyser);
         analyser.connect(audioCtx.destination);
-        analyser.fftSize = 32;
+        analyser.fftSize = 64;
 
         const bufferLength = analyser.frequencyBinCount;
         const dataArray = new Uint8Array(bufferLength);
@@ -30,28 +30,25 @@ window.onload = function() {
         const barWidth = (WIDTH / bufferLength) * 1.5
 
         let barHeight;
-        let x = 0;
+
 
         
         function renderFrame() {
             requestAnimationFrame(renderFrame);
 
-            x = 0;
-
             analyser.getByteFrequencyData(dataArray);
 
-            ctx.fillStyle = "rgba(0,0,0,1)";
+            ctx.fillStyle = "rgba(0,0,0,0.08)";
             ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-            let bars = 16;
+            let bars = 36;
 
             for (let i = 0; i < bars; i++) {
-                barHeight = (dataArray[i] * 2.8);
+                barHeight = (dataArray[i] * 2);
 
-                ctx.fillStyle = 'rgb(200, 150, 80)';
-                ctx.fillRect(x, (HEIGHT - barHeight), barWidth, barHeight);
+                ctx.fillStyle = `rgb(${i*10}, ${barHeight}, ${barHeight*2%200})`;
+                ctx.fillRect(barHeight*3, barWidth + barHeight, barHeight, barHeight);
                 
-                x += barWidth + 10
             }
         }
         audio.play();
